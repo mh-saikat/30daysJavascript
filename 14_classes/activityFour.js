b60c511feea5ebfd93025edfae674e92f9ac9ce8
mh-saikat <mr.saikat3pp@gmail.com>
2024-08-15 13:21:10 +0600

    Update syntax added.

--- 14_classes/activityFour.js
+++ 14_classes/activityFour.js
@@ -1,39 +1,54 @@
 class person {
-    constructor(name){
-        this.name = name
-    }
-    //Task 7
-    get name(){
-        return `${this._name.toUpperCase()}`
-    }
-    //Task 8
-    set name(value){
-        this._name = value
-    }
+  constructor(name) {
+    this.name = name;
+  }
+  //Task 7
+  get name() {
+    return `${this._name.toUpperCase()}`;
+  }
+  //Task 8
+  set name(value) {
+    this._name = value;
+  }
 }
 
-const fullname = new person("MH Saikat")
+const fullname = new person("MH Saikat");
 console.log(fullname);
 
+//Update syntax
 
+class updatePerson {
+  #name;
+  constructor(name) {
+    this.#name = name;
+  }
 
-class personOne {
-    constructor(firstname, lastname){
-        this.firstname = firstname;
-        this.lastname = lastname
-    }
-
-    get fullname(){
-        let fullname = `${this.firstname}${this.lastname}`
-        return fullname
-    }
-
-    set fullname(value){
-        this.fullname = value
-    }
+  get name() {
+    return this.#name.toUpperCase();
+  }
+  set name(value) {
+    this.#name = value;
+  }
 }
 
-const chai = new personOne("Ch","ai")
+const user = new updatePerson("Saikat Khan");
+console.log(user.name);
+
+class personOne {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+
+  get fullname() {
+    let fullname = `${this.firstname}${this.lastname}`;
+    return fullname;
+  }
+
+  set fullname(value) {
+    this.fullname = value;
+  }
+}
+
+const chai = new personOne("Ch", "ai");
 console.log(chai.fullname);
-
-
